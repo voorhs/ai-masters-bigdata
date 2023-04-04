5585af49f16e46777f5b7d2674c0e862de3b9ed3
voorhs <ilya_alekseev_2016@list.ru>
2023-04-04 15:12:55 +0000

    finally?..

--- projects/3/shortest_path.py
+++ projects/3/shortest_path.py
@@ -117,4 +117,4 @@ ans_schema = StructType([
 path = collected_data.where(collected_data.vertex == v_to).first()['path'] + [v_to]
 ans_df = spark.createDataFrame(data=[[path]], schema=ans_schema)
 ans_df = ans_df.withColumn('ppath', F.concat_ws(',', ans_df['path']))
-ans_df.select('ppath').write.text(sys.argv[4] + '/answer.csv')
+ans_df.select('ppath').write.text(sys.argv[4])
